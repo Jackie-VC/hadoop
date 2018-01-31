fc61790a2bdc948b61cd39500d3fcc63573fd11a
Jackie <longestar@hotmail.com>
2018-01-30 22:54:53 -0600

    average computation algrithm (in mapper)

---
+++ src/main/java/org/myorg/WordCountAverageInMapper.java
@@ -0,0 +1,94 @@
+package org.myorg;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class WordCountAverageInMapper {
+
+  public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    private Text word = new Text();
+    private static String firstToken;
+    private static int lastToken;
+    private static java.util.Map<String, Integer> combiner = new HashMap<>();
+
+    @Override
+    public void map(LongWritable key, Text value, Context context)
+        throws IOException, InterruptedException {
+
+      String line = value.toString();
+      StringTokenizer tokenizer = new StringTokenizer(line);
+      String nextToken = "";
+      while (tokenizer.hasMoreTokens()) {
+        nextToken = tokenizer.nextToken();
+        if (firstToken == null) {
+          firstToken = nextToken;
+        }
+        lastToken = Integer.parseInt(nextToken);
+      }
+      if (combiner.containsKey(firstToken)) {
+        lastToken = lastToken + combiner.get(firstToken);
+      }
+      combiner.put(firstToken, lastToken);
+      word.set(firstToken);
+      context.write(word, new IntWritable(combiner.get(firstToken)));
+    }
+  }
+
+  public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values,
+        Context context
+    ) throws IOException, InterruptedException {
+      int average = 0;
+      int count = 1;
+      for (IntWritable val : values) {
+        average = (average + val.get()) / (count + 1);
+        count++;
+      }
+      result.set(average);
+      context.write(key, result);
+    }
+  }
+
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+
+    Job job = new Job(conf, "wordcount");
+    job.setJarByClass(WordCountAverageInMapper.class);
+
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+
+    job.setMapperClass(Map.class);
+    job.setReducerClass(Reduce.class);
+
+    job.setInputFormatClass(TextInputFormat.class);
+    job.setOutputFormatClass(TextOutputFormat.class);
+
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+    job.waitForCompletion(true);
+  }
+
+
+}
+
